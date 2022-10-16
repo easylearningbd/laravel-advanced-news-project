b7cee6ee3270d11f7c6af20730f896d89158f1bd
easylearningbd <elebdsoft@gmail.com>
2022-10-17 02:59:25 +0600

    19 Adding Toster In For View Message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -65,7 +65,12 @@ class AdminController extends Controller
 
         $data->save();
 
-        return redirect()->back();
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+
+        );
+        return redirect()->back()->with($notification);
 
     } // End Method 
 
