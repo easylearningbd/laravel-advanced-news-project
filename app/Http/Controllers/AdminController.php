63feea6c33ebc6f85fae82fd3c0cf97115c3fd04
easylearningbd <elebdsoft@gmail.com>
2022-10-17 01:36:50 +0600

    15 Admin Profile & Image Update Part 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
-{
+{ 
     public function AdminDashboard(){
         return view('admin.index');
     } // End Method 
@@ -32,11 +33,17 @@ class AdminController extends Controller
 
     public function AdminLogoutPage(){
          return view('admin.admin_logout');
-    }// End Method 
+    } // End Method 
 
 
 
+    public function AdminProfile(){
 
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+
+    } // End Method 
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,17 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
+
+
+
 Route::middleware(['auth','role:admin'])->group(function() {
 
 
 Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 
 Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
+Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 
 }); // End Admin Middleware 
 
