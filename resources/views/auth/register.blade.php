eff53c97af3fb39fb3ed981889fc038a26aeff1a
easylearningbd <elebdsoft@gmail.com>
2022-10-20 01:57:16 +0600

    25 Frontend Template Register Page Setup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,64 +1,96 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('frontend.home_dashboard')
+@section('home') 
 
-        <form method="POST" action="{{ route('register') }}">
+<div class="container">
+
+<div class="row">
+<div class="col-lg-6 col-md-12">
+<div class="contact-wrpp">
+<h4 class="contactAddess-title text-center">
+Register </h4>
+<div role="form" class="wpcf7" id="wpcf7-f437-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+
+
+ <form method="POST" action="{{ route('register') }}">
             @csrf
+            
+<div style="display: none;">
+ 
+</div>
 
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
+<div class="main_section">
+<div class="row">
+ 
 
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
 
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
-            </div>
+<div class="col-md-12 col-sm-12">
+<div class="contact-title ">
+Name *
+</div>
+<div class="contact-form">
+<span class="wpcf7-form-control-wrap sub_title">
+    <input type="text" name="name" id="name" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Name"></span>
+</div>
+</div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+<div class="col-md-12 col-sm-12">
+<div class="contact-title ">
+Email *
+</div>
+<div class="contact-form">
+<span class="wpcf7-form-control-wrap sub_title">
+    <input type="email" name="email" id="email" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Email"></span>
+</div>
+</div>
 
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+<div class="col-md-12 col-sm-12">
+<div class="contact-title ">
+Password *
+</div>
+<div class="contact-form">
+<span class="wpcf7-form-control-wrap sub_title">
+    <input type="password" id="password" name="password" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Password"></span>
+</div>
+</div>
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
 
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
+<div class="col-md-12 col-sm-12">
+<div class="contact-title ">
+Confirm Password *
+</div>
+<div class="contact-form">
+<span class="wpcf7-form-control-wrap sub_title">
+    <input type="password" id="password_confirmation" name="password_confirmation" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Confirm Password"></span>
+</div>
+</div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+ 
+</div>
+ 
+ 
+ 
+ 
+<div class="row">
+<div class="col-md-12">
+<div class="contact-btn">
+<input type="submit" value="Register Now" class="wpcf7-form-control has-spinner wpcf7-submit"><span class="wpcf7-spinner"></span>
+</div>
+</div>
+</div>
+</div>
+ </form>
 
-                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+</div>
+</div>
+</div>
+</div>
+</div>
 
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+
+
+@endsection
